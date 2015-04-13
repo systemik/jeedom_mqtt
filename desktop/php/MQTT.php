097d7f4de70f08382edcf32b12c5e179ae90e009
Cédric Dupont <lunarok@gmail.com>
2015-04-13 15:42:13 +0200

    Update MQTT.php

--- desktop/php/MQTT.php
+++ desktop/php/MQTT.php
@@ -29,9 +29,9 @@ $eqLogics = eqLogic::byType('MQTT');
             <div class="eqLogicThumbnailContainer">
                       <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
            <center>
-            <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+            <i class="fa fa-plus-circle" style="font-size : 7em;color:#00979c;"></i>
         </center>
-        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
     </div>
                 <?php
                 foreach ($eqLogics as $eqLogic) {
